7205b10ebe583ceaec780a2214007be417e57644
Sumeet Gupta <sumeetgupta3690@gmail.com>
2024-08-17 22:06:32 +0530

    Users and Posts API

--- server/index.js
+++ server/index.js
@@ -13,8 +13,14 @@ import morgan from 'morgan' //useful for monitoring and debugging request detail
 import path from "path"
 import { fileURLToPath } from 'url'
 
+// Importing Routes
 import authRoutes from './routes/auth.js'
+import userRoutes from './routes/users.js'
+import postRoutes from './routes/posts.js'
 import {register} from './controllers/auth.js'
+import {createPost} from './controllers/posts.js'
+
+import { verifyToken } from './middleware/auth.js'
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url) //Grab File URL - Only Works on type module
@@ -49,9 +55,12 @@ const upload = multer({storage})
 
 // ROUTES WITH FILES
 app.post('/auth/register', upload.single('picture'), register)
+app.post('/posts', verifyToken, upload.single('picture'), createPost)
 
 // ROUTES
 app.use("/auth", authRoutes)
+app.use("/user", userRoutes)
+app.use("/post", postRoutes)
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001
