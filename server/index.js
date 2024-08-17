2ff5a2bc7d8bda5a84bd6ebdc55e9e1fc845b08d
Sumeet Gupta <sumeetgupta3690@gmail.com>
2024-08-17 10:08:46 +0530

    MongoDB Connection

--- server/index.js
+++ server/index.js
@@ -43,3 +43,16 @@ const storage = multer.diskStorage({
 })
 
 const upload = multer({storage})
+
+// MONGOOSE SETUP
+const PORT = process.env.PORT || 6001
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() =>{
+    app.listen(PORT, () => console.log(`SERVER Port : ${PORT}`))
+    
+})
+.catch((error) =>{
+    console.log(error);
+    
+})
